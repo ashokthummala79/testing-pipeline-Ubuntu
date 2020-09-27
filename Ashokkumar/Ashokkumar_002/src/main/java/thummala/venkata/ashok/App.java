369e44f1d122e614c58050d3846f9e2b03760ce4
ashok <ashokemcstorage@gmail.com>
2020-09-27 14:10:54 +0530

    adding Ashokkumar

---
+++ Ashokkumar/Ashokkumar_002/src/main/java/thummala/venkata/ashok/App.java
@@ -0,0 +1,13 @@
+package thummala.venkata.ashok;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
