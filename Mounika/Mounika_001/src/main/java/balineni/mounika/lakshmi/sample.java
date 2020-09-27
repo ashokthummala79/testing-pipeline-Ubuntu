22c804b569136eaa5bc58a4ea7705b03dfc35f55
ashok <ashokemcstorage@gmail.com>
2020-09-27 14:16:15 +0530

    adding Mounika

---
+++ Mounika/Mounika_001/src/main/java/balineni/mounika/lakshmi/sample.java
@@ -0,0 +1,13 @@
+package balineni.mounika.lakshmi;
+
+/**
+ * Hello world!
+ *
+ */
+public class sample 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Mounika/Mounika_003/src/main/java/balineni/mounika/lakshmi/App.java
@@ -0,0 +1,13 @@
+package balineni.mounika.lakshmi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
