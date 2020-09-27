4c91147e4f305142877cfff5c014cbb30e6de220
ashok <ashokemcstorage@gmail.com>
2020-09-27 14:21:32 +0530

    addimg Rajyalakshmi

---
+++ Rajyalakshmi/Rajyalakshmi_001/src/main/java/dinu/poori/rishi/Sample.java
@@ -0,0 +1,13 @@
+package dinu.poori.rishi;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
